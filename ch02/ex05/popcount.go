8de97334f29a6577e79eb7e7f33deb3c50ae8be5
chiiia12 <llnsbchsep12@gmail.com>
2017-11-23 00:57:06 +0900

    ch02/ex05

--- ch02/ex05/popcount.go
+++ ch02/ex05/popcount.go
@@ -1,27 +1,13 @@
 package popcount
 
-var pc [256]byte
-
 func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i%1)
-	}
-}
-func PopCountNormal(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
 }
 
-func PopCount64(x uint64) int {
-	var tmp byte = 0
-	for i := 0; i < 64; i++ {
-		tmp += pc[byte(x>>uint64(i))]
+func PopCountEx05(x uint64) int {
+	var count = 0
+	for ; x > 0; {
+		x = x & (x - 1)
+		count++
 	}
-	return int(tmp)
+	return count
 }
--- ch02/ex05/popcount_test.go
+++ ch02/ex05/popcount_test.go
@@ -2,12 +2,18 @@ package popcount
 
 import (
 	"testing"
+	"fmt"
 	"gopl.io/ch2/popcount"
 )
 
-func BenchmarkPopCount64(b *testing.B) {
-	popcount.PopCount(64)
+func BenchmarkPopCountEx05(b *testing.B) {
+	PopCountEx05(64)
 }
-func TestPopCount64(t *testing.T) {
-	PopCount64(64)
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount(64)
+	}
+}
+func TestPopCount(t *testing.T) {
+	fmt.Println(`popcount is `, PopCountEx05(16))
 }
