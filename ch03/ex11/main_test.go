e696fd88ba3f4e7f52dc7f0521f7686077485889
chiiia12 <llnsbchsep12@gmail.com>
2017-12-10 21:25:34 +0900

    ch03/ex11 add testcase

--- ch03/ex11/main_test.go
+++ ch03/ex11/main_test.go
@@ -5,17 +5,16 @@ import (
 )
 
 func TestComma(t *testing.T) {
-	parameters :=[]struct {
-		input,expected string
+	parameters := []struct {
+		input, expected string
 	}{
-		{"123","123"},{"1234","1,234"},{"1234567","1,234,567"},
-		{"123.123","123.123"},{"1234.1234","1,234.1234"},
+		{"123", "123"}, {"1234", "1,234"}, {"1234567", "1,234,567"},
+		{"123.123", "123.123"}, {"1234.1234", "1,234.1234"}, {"-1234.1234", "-1,234.1234"},
 	}
-	for i:=range parameters{
-		actual :=comma(parameters[i].input)
-		if actual !=parameters[i].expected{
-			t.Errorf("comma[%s] is not %s actual is %s",parameters[i].input,parameters[i].expected,actual)
+	for i := range parameters {
+		actual := comma(parameters[i].input)
+		if actual != parameters[i].expected {
+			t.Errorf("comma[%s] is not %s actual is %s", parameters[i].input, parameters[i].expected, actual)
 		}
 	}
 }
-
