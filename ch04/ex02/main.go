ddf00ab958d38c6c42eed2eec8bba793e447d097
chiiia12 <llnsbchsep12@gmail.com>
2017-12-13 23:33:00 +0900

    ch04/ex02 add flag

--- ch04/ex02/main.go
+++ ch04/ex02/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"os"
 	"crypto/sha256"
 	"fmt"
+	"crypto/sha512"
+	"flag"
+)
+
+var (
+	optSha384 = flag.Bool("a", false, "option -a use SHA384")
+	optSha512 = flag.Bool("b", false, "option -b use SHA512")
+	str       = flag.String("s", "", "option -s is string")
 )
 
 func main() {
+	flag.Parse()
 
-	for i := 1; i < len(os.Args); i++ {
-		printSha256(os.Args[i])
+	if *optSha384 {
+		printSha384(*str)
+	} else if *optSha512 {
+		printSha512(*str)
+	} else {
+		printSha256(*str)
 	}
 
 }
 func printSha256(s string) {
 	fmt.Printf("%x\n", sha256.Sum256([]byte(s)))
+}
+func printSha384(s string) {
+	fmt.Printf("%x\n", sha512.Sum384([]byte(s)))
+}
+func printSha512(s string) {
+	fmt.Printf("%x\n", sha512.Sum512([]byte(s)))
 }
