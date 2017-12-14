b65b7bb52a9d5c5a364777d9130af96256b7e087
chiiia12 <llnsbchsep12@gmail.com>
2017-12-14 22:34:38 +0900

    tmp/slice

---
+++ tmp/ch04.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5}
+	fmt.Printf("%p\n",s)//0xc420016210
+	fmt.Printf("%p\n",s[:2])//0xc420016210
+	reverse(s[:2])
+	fmt.Println(s)
+	reverse(s[2:])
+	fmt.Println(s)
+	reverse(s)
+	fmt.Println(s)
+}
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
