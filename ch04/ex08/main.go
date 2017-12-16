f7b2ffe3e66121a543d2e521285ce8b95dec365f
chiiia12 <llnsbchsep12@gmail.com>
2017-12-16 19:04:58 +0900

    ch04/ex08 wip

---
+++ ch04/ex08/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := map[string]int{
+		"control": 0,
+		"digit":   0,
+		"graphic": 0,
+		"letter":  0,
+		"lower":   0,
+		"mark":    0,
+		"number":  0,
+		"print":   0,
+		"punct":   0,
+		"space":   0,
+		"symbol":  0,
+		"title":   0,
+		"upper":   0,
+	}
+	//counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		fmt.Printf("%v %q %v\n", string(r), n, err)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		if unicode.IsControl(r){
+			counts["control"]++
+			continue
+		}
+		if unicode.IsDigit(r){
+			counts["digit"]++
+			continue
+		}
+		if unicode.IsGraphic(r){
+			counts["graphic"]++
+			continue
+		}
+		if unicode.IsLower(r){
+			counts["lower"]++
+			continue
+		}
+		if unicode.IsMark(r){
+			counts["mark"]++
+			continue
+		}
+		if unicode.IsLower(r){
+			counts["lower"]++
+			continue
+		}
+		if unicode.IsMark(r){
+			counts["mark"]++
+			continue
+		}
+		if unicode.IsNumber(r){
+			counts["number"]++
+			continue
+		}
+		if unicode.IsPrint(r){
+			counts["print"]++
+			continue
+		}
+		if unicode.IsPunct(r){
+			counts["punct"]++
+			continue
+		}
+		if unicode.IsSpace(r){
+			counts["space"]++
+			continue
+		}
+		if unicode.IsSymbol(r){
+			counts["symbol"]++
+			continue
+		}
+		if unicode.IsTitle(r){
+			counts["title"]++
+			continue
+		}
+		if unicode.IsUpper(r){
+			counts["upper"]++
+			continue
+		}
+
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
