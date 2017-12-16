5f0d2fc97f1c8a646298826f427b26a4b82ae8c0
chiiia12 <llnsbchsep12@gmail.com>
2017-12-17 02:47:16 +0900

    ch04/ex11 wip add createissue

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gopl.io/ch4/github"
+	"net/http"
+	"net/url"
+	"os"
+	"io/ioutil"
 )
 
 var (
@@ -10,14 +15,38 @@ var (
 	//stringのポインタが返ってくる
 )
 
+const (
+	createIssuesURL = "https://api.github.com/repos/chiiia12/golangstudy"
+)
+
 func main() {
 	flag.Parse()
 	switch *command {
 	case "create":
-		fmt.Println("create command selected")
+		createIssue(os.Args[2])
 	case "show":
+		github.SearchIssues([]string{"repo:chiiia12/golangstudy", "is:open", "json", "decoder"})
 		fmt.Println("show command selected")
 	case "update":
 		fmt.Println("update command selected")
 	}
+}
+
+func createIssue(title string) {
+	//TODO:headerにtokenとcontenttypeつける
+	//TODO:respをmappingするstructつくる
+	values := url.Values{}
+	values.Add("title", title)
+	resp, err := http.PostForm(createIssuesURL, values)
+	fmt.Println("postForm")
+	if err != nil {
+		fmt.Println("create is failed", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		fmt.Errorf("status code is not OK.status is %v", resp.Status)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("create is success", b)
 }
