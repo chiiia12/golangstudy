6c67e4e7c601ba7d5df83b5445e078cae2f1a4d3
chiiia12 <llnsbchsep12@gmail.com>
2017-12-17 10:24:58 +0900

    ch04/ex11 close issue

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -14,6 +14,7 @@ var (
 	command = flag.String("command", "", "option -command create/show/update")
 	token   = flag.String("token", "", "option -token is github generated token")
 	title   = flag.String("title", "", "option -title is issue's title")
+	issue   = flag.String("issue", "", "option -issue is number of issue ")
 	//stringのポインタが返ってくる
 )
 
@@ -28,15 +29,50 @@ func main() {
 		createIssue()
 	case "show":
 		github.SearchIssues([]string{"repo:chiiia12/golangstudy", "is:open", "json", "decoder"})
-		fmt.Println("show command selected")
 	case "update":
 		fmt.Println("update command selected")
+	case "close":
+		fmt.Println("close command selected")
+		closeIssue()
 	}
+}
+func closeIssue() {
+	fmt.Println("token is ", *token)
+	strJson := `{
+		"state":"close"
+	}`
+
+	req, err := http.NewRequest("PATCH", createIssuesURL + "/issues/" + *issue, bytes.NewBuffer([]byte(strJson)))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer " + *token)
+
+	client := &http.Client{Timeout: time.Duration(10 * time.Second)}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("create is failed", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Errorf("status code is not OK.status is %v", resp.Status)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("readAll is err", err)
+	}
+	fmt.Println("close is success", string(b))
+
 }
 
 func createIssue() {
 	fmt.Println("token is ", *token)
-	strJson :=`{
+	strJson := `{
 	"title": "Found a bug",
 		"body": "I'm having a problem with this.",
 		"name":"repo name",
