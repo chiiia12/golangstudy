789199016d896b38a08acf2dc6b0176b5e0348ca
chiiia12 <llnsbchsep12@gmail.com>
2017-12-19 21:17:34 +0900

    ch03/ex07

---
+++ ch03/ex07/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, newton(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.YCbCr{255 - contrast*n, 255 - contrast*n*2, 255 - contrast*n*2}
+		}
+	}
+	//return color.YCbCr{255 - contrast,255-contrast,255-contrast}
+	return color.Black
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.RGBA{0, 255-contrast*i, 0, 0xff}
+
+		}
+	}
+	//return color.RGBA{255, 255, 255, 0}
+	return color.Black
+}
