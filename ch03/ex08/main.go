c1c104d748870218044756bb2c84d02019b2e5d7
chiiia12 <llnsbchsep12@gmail.com>
2017-12-21 14:42:14 +0900

    ch03/ex08 add switch datatype flag

--- ch03/ex08/main.go
+++ ch03/ex08/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"math/big"
 	"math/cmplx"
+	"flag"
 )
 
 type ComplexFloat struct {
@@ -14,7 +15,15 @@ type ComplexFloat struct {
 	im big.Float
 }
 
+type ComplexRat struct {
+	re big.Rat
+	im big.Rat
+}
+
+var data = flag.String("data", "complex64", "select data type :complex64/complex128/big.Float/big.Rat")
+
 func main() {
+	flag.Parse()
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
@@ -26,21 +35,33 @@ func main() {
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			// Image point (px, py) represents complex value z.
-			//complex64
-			zcomplex := complex(x, y)
-			//img.Set(px, py, mandelbrotComplex64(complex64(zcomplex)))
 
-			//complex128
-			img.Set(px, py, mandelbrotComplex128(zcomplex))
-
-			///big.Float
-			//xFloat := new(big.Float)
-			//xFloat.SetFloat64(x)
-			//yFloat := new(big.Float)
-			//yFloat.SetFloat64(y)
-			//zfloat := ComplexFloat{re: *xFloat, im: *yFloat}
-			//img.Set(px, py, mandelbrotFloat(zfloat))
-
+			switch *data {
+			case "complex64":
+				//complex64
+				zcomplex := complex(x, y)
+				img.Set(px, py, mandelbrotComplex64(complex64(zcomplex)))
+			case "complex128":
+				//complex128
+				zcomplex := complex(x, y)
+				img.Set(px, py, mandelbrotComplex128(zcomplex))
+			case "big.Float":
+				///big.Float
+				xFloat := new(big.Float)
+				xFloat.SetFloat64(x)
+				yFloat := new(big.Float)
+				yFloat.SetFloat64(y)
+				zfloat := ComplexFloat{re: *xFloat, im: *yFloat}
+				img.Set(px, py, mandelbrotFloat(zfloat))
+			case "big.Rat":
+				//big.Rat
+				xRat := new(big.Rat)
+				xRat.SetFloat64(x)
+				yRat := new(big.Rat)
+				yRat.SetFloat64(y)
+				zRat := ComplexRat{re: *xRat, im: *yRat}
+				img.Set(px, py, mandelbrotRat(zRat))
+			}
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
@@ -71,6 +92,42 @@ func mandelbrotFloat(z ComplexFloat) color.Color {
 
 		//絶対値を出す
 		var a, b big.Float
+		a.Mul(&v.re, &v.re)
+		b.Mul(&v.im, &v.im)
+		res.Add(&a, &b)
+
+		if res.Cmp(&absValSq) > 0 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrotRat(z ComplexRat) color.Color {
+	const iterations = 64
+	const contrast = 15
+	const absVal = 2
+	var absValSq big.Rat
+	absValSq.SetInt64(absVal * absVal)
+	var v ComplexRat
+	for n := uint8(0); n < iterations; n++ {
+		var aa, ab, bb, res big.Rat
+		//v = v*v + z
+		//v*v
+		//(a+bi)(a+bi)=a^2+2abi-b^2
+		aa.Mul(&v.re, &v.re)
+		ab.Mul(&v.re, &v.im)
+		bb.Mul(&v.im, &v.im)
+		//v*v+z
+		//(a+bi)(a+bi)+(c+di)=(a^2+b^2+c)+(2ab+c)i
+		v.re.Sub(&aa, &bb)
+		v.im.Add(&ab, &ab)
+
+		v.re.Add(&v.re, &z.re)
+		v.im.Add(&v.im, &z.im)
+
+		//絶対値を出す
+		var a, b big.Rat
 		a.Mul(&v.re, &v.re)
 		b.Mul(&v.im, &v.im)
 		res.Add(&a, &b)
--- ch03/ex08/run.bash
+++ ch03/ex08/run.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-go run main.go >output.png
+go run main.go -data "complex128" >output.png
