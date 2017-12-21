4298fcef5e62a6112a6c8b877ec4a71fcfdcc455
chiiia12 <llnsbchsep12@gmail.com>
2017-12-21 14:32:49 +0900

    ch03/ex08 add complex64/complex128

--- ch03/ex08/main.go
+++ ch03/ex08/main.go
@@ -6,6 +6,7 @@ import (
 	"image/png"
 	"os"
 	"math/big"
+	"math/cmplx"
 )
 
 type ComplexFloat struct {
@@ -21,20 +22,31 @@ func main() {
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := new(big.Float)
-		y.SetFloat64(float64(py)/height*(ymax-ymin) + ymin)
+		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := new(big.Float)
-			x.SetFloat64(float64(px)/width*(xmax-xmin) + xmin)
-			z := ComplexFloat{re: *x, im: *y}
+			x := float64(px)/width*(xmax-xmin) + xmin
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			//complex64
+			zcomplex := complex(x, y)
+			//img.Set(px, py, mandelbrotComplex64(complex64(zcomplex)))
+
+			//complex128
+			img.Set(px, py, mandelbrotComplex128(zcomplex))
+
+			///big.Float
+			//xFloat := new(big.Float)
+			//xFloat.SetFloat64(x)
+			//yFloat := new(big.Float)
+			//yFloat.SetFloat64(y)
+			//zfloat := ComplexFloat{re: *xFloat, im: *yFloat}
+			//img.Set(px, py, mandelbrotFloat(zfloat))
+
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
 }
 
-func mandelbrot(z ComplexFloat) color.Color {
+func mandelbrotFloat(z ComplexFloat) color.Color {
 	const iterations = 64
 	const contrast = 15
 	const absVal = 2
@@ -42,7 +54,7 @@ func mandelbrot(z ComplexFloat) color.Color {
 	absValSq.SetInt64(absVal * absVal)
 	var v ComplexFloat
 	for n := uint8(0); n < iterations; n++ {
-		var aa, ab, bb,res big.Float
+		var aa, ab, bb, res big.Float
 		//v = v*v + z
 		//v*v
 		//(a+bi)(a+bi)=a^2+2abi-b^2
@@ -51,17 +63,17 @@ func mandelbrot(z ComplexFloat) color.Color {
 		bb.Mul(&v.im, &v.im)
 		//v*v+z
 		//(a+bi)(a+bi)+(c+di)=(a^2+b^2+c)+(2ab+c)i
-		v.re.Sub(&aa,&bb)
-		v.im.Add(&ab,&ab)
+		v.re.Sub(&aa, &bb)
+		v.im.Add(&ab, &ab)
 
-		v.re.Add(&v.re,&z.re)
-		v.im.Add(&v.im,&z.im)
+		v.re.Add(&v.re, &z.re)
+		v.im.Add(&v.im, &z.im)
 
 		//絶対値を出す
-		var a,b big.Float
-		a.Mul(&v.re,&v.re)
-		b.Mul(&v.im,&v.im)
-		res.Add(&a,&b)
+		var a, b big.Float
+		a.Mul(&v.re, &v.re)
+		b.Mul(&v.im, &v.im)
+		res.Add(&a, &b)
 
 		if res.Cmp(&absValSq) > 0 {
 			return color.Gray{255 - contrast*n}
@@ -69,4 +81,30 @@ func mandelbrot(z ComplexFloat) color.Color {
 	}
 	return color.Black
 }
+func mandelbrotComplex64(z complex64) color.Color {
 
+	const iterations = 64
+	const contrast = 15
+	var v complex64
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		re, im := real(v), imag(v)
+		if re*re+im*im > 4 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+func mandelbrotComplex128(z complex128) color.Color {
+
+	const iterations = 200
+	const contrast = 15
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 4 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
