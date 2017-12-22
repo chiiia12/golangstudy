c5b17be1e3b5923a6f9142074a32cf99b2ab44ee
chiiia12 <llnsbchsep12@gmail.com>
2017-12-22 12:10:29 +0900

    ch04/ex03 slice to array

--- ch04/ex03/main.go
+++ ch04/ex03/main.go
@@ -2,23 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	s :=[]int{0,1,2,3,4,5}
-
-	reverse(toPtr(s[:2]))
-	fmt.Println(s)
-	reverse(toPtr(s[2:]))
-	fmt.Println(s)
-	reverse(toPtr(s))
+func main() {
+	s := [6]int{0, 1, 2, 3, 4, 5}
+	s = reverse(&s)
 	fmt.Println(s)
 }
 
-func reverse(s *[]int) {
-	var arr []int =*s
+func reverse(s *[6]int) [6]int {
+	var arr = *s
 	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
-}
-func toPtr(arr []int)*[]int{
-	return &arr
+	return arr
 }
