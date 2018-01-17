308e33de38d7f6aa8b8f58b158bc11aebbdcb621
chiiia12 <llnsbchsep12@gmail.com>
2018-01-17 13:37:17 +0900

    ch05/ex17

--- ch05/ex17/main.go
+++ ch05/ex17/main.go
@@ -7,27 +7,34 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://golang.org")
+	resp, err := http.Get("https://recruit-tech.co.jp/")
 	if err != nil {
 		return
 	}
 	doc, err := html.Parse(resp.Body)
 	images := ElementsByTagName(doc, "img")
 	heading := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
-	fmt.Println(images, heading)
+	for _, v := range images {
+		fmt.Println(v.Data, v.Attr)
+	}
+	for _, v := range heading {
+		fmt.Println(v.Data, v.Attr)
+	}
 }
 
 func ElementsByTagName(doc *html.Node, name ...string) []*html.Node {
-	//TODO:動かない気がする。つづきから
-	var d *html.Node
-	for _, v := range name {
-		if doc.Data == v {
-			d = append(d, doc)
+	var d []*html.Node
+	if doc.Type == html.ElementNode {
+		for _, v := range name {
+			//fmt.Println(doc.Data, v)
+			if doc.Data == v {
+				d = append(d, doc)
+			}
 		}
 	}
 	for c := doc.FirstChild; c != nil; c = c.NextSibling {
-		ElementsByTagName(c, name)
+		d = append(d, ElementsByTagName(c, name...)...)
+		//fmt.Println(ElementsByTagName(c, name...))
 	}
 	return d
-
 }
