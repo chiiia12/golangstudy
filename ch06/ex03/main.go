e62b7df57c4419de26dad446f9485eed7a6501ec
chiiia12 <llnsbchsep12@gmail.com>
2018-01-20 15:26:16 +0900

    ch06/ex03

---
+++ ch06/ex03/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+)
+
+func main() {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(2)
+	x.Add(5)
+	y.Add(1)
+	y.Add(2)
+	y.Add(3)
+	y.Add(4)
+
+	fmt.Println(x.String())
+	fmt.Println(y.String())
+	//x.UnionWith(&y)
+	//x.IntersectWith(&y)
+	x.DifferenceWith(&y)
+	fmt.Println(x.String())
+}
+
+type IntSet struct {
+	words []uint
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+//積集合
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, v := range s.words {
+		s.words[i] = v & t.words[i]
+	}
+}
+
+//差集合
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, v := range s.words {
+		intersect := v & t.words[i]
+		s.words[i] = v & ^intersect
+	}
+}
+
+//対照差
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	for i, v := range s.words {
+		tmp := v | t.words[i]
+		s.words[i] = tmp &^ (v & t.words[i])
+	}
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
---
+++ ch06/ex03/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+
+func TestIntersectWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(2)
+	x.Add(3)
+	y.Add(3)
+	y.Add(4)
+	y.Add(5)
+	x.IntersectWith(&y)
+	if actual := x.String(); actual != "{3}" {
+		t.Errorf("intersectWith result isnot {3},actual %v", actual)
+	}
+}
+func TestDifferenceWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(2)
+	x.Add(3)
+	y.Add(3)
+	y.Add(4)
+	y.Add(5)
+	x.DifferenceWith(&y)
+	if actual := x.String(); actual != "{1 2}" {
+		t.Errorf("differenceWith result isnot {1 2},actual %v", actual)
+	}
+}
+func TestSymmetricDifference(t *testing.T) {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(2)
+	x.Add(3)
+	y.Add(3)
+	y.Add(4)
+	y.Add(5)
+	x.SymmetricDifference(&y)
+	if actual := x.String(); actual != "{1 2 4 5}" {
+		t.Errorf("SymmetricDifference result isnot {1 2 4 5},actual %v", actual)
+	}
+
+}
