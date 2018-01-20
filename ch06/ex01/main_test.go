3cd057b848d4281914f999a310abde828a3ea83b
chiiia12 <llnsbchsep12@gmail.com>
2018-01-20 14:19:51 +0900

    ch06/ex01 fix format

--- ch06/ex01/main_test.go
+++ ch06/ex01/main_test.go
@@ -9,7 +9,7 @@ func TestAdd(t *testing.T) {
 	x.Add(1)
 	x.Add(144)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("add result is not {1 144}", actual)
+		t.Errorf("add result is not {1 144} %v", actual)
 	}
 }
 func TestRemove(t *testing.T) {
@@ -17,11 +17,11 @@ func TestRemove(t *testing.T) {
 	x.Add(1)
 	x.Add(144)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("add result is not {1 144}", actual)
+		t.Errorf("add result is not {1 144} %v", actual)
 	}
 	x.Remove(1)
 	if actual := x.String(); actual != "{144}" {
-		t.Errorf("remove result is not {144}", actual)
+		t.Errorf("remove result is not {144} %v", actual)
 	}
 }
 
@@ -30,11 +30,11 @@ func TestClear(t *testing.T) {
 	x.Add(1)
 	x.Add(144)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("add result is not {1 144}", actual)
+		t.Errorf("add result is not {1 144} %v", actual)
 	}
 	x.Clear()
 	if actual := x.String(); actual != "{}" {
-		t.Errorf("clear result is not {}", actual)
+		t.Errorf("clear result is not {} %v", actual)
 	}
 
 }
@@ -43,15 +43,15 @@ func TestCopy(t *testing.T) {
 	x.Add(1)
 	x.Add(144)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("add result is not {1 144}", actual)
+		t.Errorf("add result is not {1 144} %v", actual)
 	}
 	z := x.Copy()
 	z.Add(2)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("copy result x is not {1 144}", actual)
+		t.Errorf("copy result x is not {1 144} %v", actual)
 	}
 	if actual := z.String(); actual != "{1 2 144}" {
-		t.Errorf("copy result z is not {1 2 144}", actual)
+		t.Errorf("copy result z is not {1 2 144} %v", actual)
 	}
 }
 func TestLen(t *testing.T) {
@@ -59,9 +59,9 @@ func TestLen(t *testing.T) {
 	x.Add(1)
 	x.Add(144)
 	if actual := x.String(); actual != "{1 144}" {
-		t.Errorf("add result is not {1 144}", actual)
+		t.Errorf("add result is not {1 144} %v", actual)
 	}
 	if actual := x.Len(); actual != 2 {
-		t.Errorf("x.Len is not 2", actual)
+		t.Errorf("x.Len is not 2 %v", actual)
 	}
 }
