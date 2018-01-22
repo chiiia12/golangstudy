e85812f7e9df51b3e88a682665b65d7798864cb5
chiiia12 <llnsbchsep12@gmail.com>
2018-01-22 21:04:27 +0900

    ch07/ex05 fix

--- ch07/ex04/main.go
+++ ch07/ex04/main.go
@@ -1,18 +1,32 @@
 package main
 
-import "io"
+import (
+	"io"
+	"fmt"
+	"os"
+	"golang.org/x/net/html"
+)
 
 func main() {
-
+	r := NewReader("<a href='http://google.com>hoge</a>")
+	doc, err := html.Parse(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "html.parse: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("doc is %v\n", doc)
 }
+
 func NewReader(s string) io.Reader {
 	var result SampleReader
+	result.s = s
 	return &result
 }
 
-type SampleReader int
+type SampleReader struct {
+	s string
+}
 
 func (r *SampleReader) Read(p []byte) (n int, err error) {
-
 	return len(p), nil
 }
