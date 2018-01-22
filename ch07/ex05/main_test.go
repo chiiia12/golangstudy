2fe3239f961afd0db253a8b4679b5d2c9581a07e
chiiia12 <llnsbchsep12@gmail.com>
2018-01-22 21:05:19 +0900

    ch07/ex05

--- ch07/ex05/main_test.go
+++ ch07/ex05/main_test.go
@@ -1,1 +1,16 @@
-package ex05
+package main
+
+import (
+	"testing"
+	"strings"
+	"bytes"
+)
+
+func TestLimitReader(t *testing.T) {
+	r := strings.NewReader("Hello,World")
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(LimitReader(r, 5))
+	if actual := buf.String(); actual != "Hello" {
+		t.Errorf("LimitReader doesn't return Hello actual is [%v]", actual)
+	}
+}
