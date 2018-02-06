24c7037aa2bd1b3e94750db1c5a8de94e6f9ff43
chiiia12 <llnsbchsep12@gmail.com>
2018-02-06 14:05:29 +0900

    ch07/ex15

--- ch07/ex15/main.go
+++ ch07/ex15/main.go
@@ -7,21 +7,16 @@ import (
 
 func main() {
 	var formula string;
-	//fmt.Scan(&formula)
-	//fmt.Println(formula)
-	formula = "pow(x, 3)"
-	//TODO:変数の取得できん
-	//r := regexp.MustCompile(`[A-Za-z]`)
-	//fmt.Println(r.FindAllStringSubmatch(formula, -1))
-	var x float64
-	fmt.Println("input value for x")
-	fmt.Scan(&x)
-	env := eval.Env{"x": x}
-	exp, err := eval.Parse(formula)
-	if err != nil {
-		fmt.Printf("Parse() error is not nil.error is: %v\n", err)
-	}
-	fmt.Printf("exp is %v\n", exp)
-	fmt.Println(exp.Eval(env))
+	for {
+		fmt.Printf("input a expression:")
+		fmt.Scan(&formula)
+		fmt.Println(formula)
 
+		env := eval.Env{}
+		exp, err := eval.Parse(formula)
+		if err != nil {
+			fmt.Printf("Parse() error is not nil.error is: %v\n", err)
+		}
+		fmt.Println(exp.Eval(env))
+	}
 }
