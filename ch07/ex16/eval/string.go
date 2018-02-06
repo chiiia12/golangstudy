1fc6b45e66a95f4ae81042b4e3710dce9b018fac
chiiia12 <llnsbchsep12@gmail.com>
2018-02-06 14:22:10 +0900

    ch07/ex16 wip

---
+++ ch07/ex16/eval/string.go
@@ -0,0 +1,38 @@
+package eval
+
+import (
+	"strconv"
+	"fmt"
+	"strings"
+)
+
+func (v Var) String() string {
+	return string(v)
+}
+
+func (l literal) String() string {
+	return strconv.FormatFloat(float64(l), 'f', 4, 32)
+}
+
+func (u unary) String() string {
+	return string(u.op) + u.x.String()
+}
+
+func (b binary) String() string {
+	return b.x.String() + string(b.op) + b.y.String()
+}
+
+func (c call) String() string {
+	var joinlist []string
+	for _, v := range c.args {
+		joinlist = append(joinlist, v.String())
+	}
+	return fmt.Sprintf("%v(%v)", c.fn, strings.Join(joinlist, ","))
+}
+func (n newVar) String() string {
+	var joinlist []string
+	for _, v := range n.args {
+		joinlist = append(joinlist, v.String())
+	}
+	return fmt.Sprintf("%v(%v)", n.fn, strings.Join(joinlist, ","))
+}
--- ch07/ex16/main.go
+++ ch07/ex16/main.go
@@ -1,18 +1,32 @@
-package ex16
+package main
 
 import (
 	"net/http"
 	"log"
+	"fmt"
+	"gopl.io/ch7/eval"
 )
 
 func main() {
+	fmt.Println("ex) http://localhost:8000?expr=(1.1 * 2 %%2b 3)*3&expr=355/113")
 	http.HandleFunc("/", calc)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 func calc(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
+	r.ParseForm()
+	values, ok := r.Form["expr"]
+	if !ok {
+
 	}
-	queryParam := r.URL.Query()
+	fmt.Println(values)
+
+	for _, v := range values {
+		env := eval.Env{}
+		exp, err := eval.Parse(v)
+		if err != nil {
+			fmt.Printf("Parse error is not nil.error is :%v\n", err)
+		}
+		fmt.Fprintf(w, "answer is %v\n", exp.Eval(env))
+	}
 
 }
