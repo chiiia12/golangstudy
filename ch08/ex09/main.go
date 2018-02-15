8ae3238206de03227394eda71f4df75fbc0461d4
chiiia12 <llnsbchsep12@gmail.com>
2018-02-15 09:00:54 +0900

    ch08/ex09 du3

--- ch08/ex09/main.go
+++ ch08/ex09/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"io/ioutil"
 	"time"
+	"sync"
 )
 
 var verbose = flag.Bool("v", false, "show verbose progress messages")
@@ -18,11 +19,14 @@ func main() {
 		roots = []string{"."}
 	}
 	fileSizes := make(chan int64)
-	//一つのゴルーチンしか回ってないのか
+	var wg sync.WaitGroup
+	for _, root := range roots {
+		wg.Add(1)
+		go walkDir(root, &wg, fileSizes)
+	}
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		wg.Wait()
+		//すべてのgoroutineが終わってから閉じる
 		close(fileSizes)
 	}()
 
@@ -52,18 +56,25 @@ func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
 }
 
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
+	defer wg.Done()
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			subdir := filepath.Join(dir, entry.Name())
-			walkDir(subdir, fileSizes)
+			wg.Add(1)
+			go walkDir(subdir, wg, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
+//係数セマフォを使用しないとtoo many filesで怒られる
+var sema = make(chan struct{}, 20)
+
 func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}
+	defer func() { <-sema }()
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du1:%v\n", err)
