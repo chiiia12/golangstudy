ee19c26a6e83380bb6b31ae5aeb021e77ac2b073
chiiia12 <llnsbchsep12@gmail.com>
2018-02-16 09:02:35 +0900

    ch08/ex10 verify receive done channel

--- ch08/ex10/crawl.go
+++ ch08/ex10/crawl.go
@@ -12,7 +12,7 @@ var done = make(chan struct{})
 func canceled() bool {
 	select {
 	case <-done:
-		log.Println("canceled is done")
+		//log.Println("canceled is done")
 		return true
 	default:
 		return false
@@ -28,13 +28,28 @@ func main() {
 	worklist := make(chan []string)
 	unseenLinks := make(chan string)
 
-	go func() { worklist <- os.Args[1:] }()
+	go func() {
+		select {
+		case <-done:
+			return
+		case worklist <- os.Args[1:]:
+		}
+	}()
 
 	for i := 0; i < 20; i++ {
 		go func() {
+			if canceled() {
+				return
+			}
 			for link := range unseenLinks {
 				foundLinks := crawl(link)
-				go func() { worklist <- foundLinks }()
+				go func() {
+					select {
+					case <-done:
+						return
+					case worklist <- foundLinks:
+					}
+				}()
 			}
 		}()
 	}
@@ -43,17 +58,30 @@ func main() {
 		for _, link := range list {
 			if !seen[link] {
 				seen[link] = true
-				unseenLinks <- link
+				select {
+				case unseenLinks <- link:
+				case <-done:
+					log.Println("done get in list loop")
+					return
+				}
 			}
+			log.Println("list loop")
 		}
+		log.Println("worklist loop")
 	}
+	log.Println("worklist loop done")
+	//worklist loopが終わらなくて下のforではdoneをキャッチできない。→main関数が終わらない
 	for {
+		log.Println("channel for")
 		select {
 		case <-done:
-			for range worklist {
-
-			}
 			log.Println("main's loop is done")
+			for range worklist {
+				//channelの呼び出し
+			}
+			for range unseenLinks {
+				//channelの呼び出し
+			}
 			return
 		}
 	}
@@ -68,9 +96,9 @@ func crawl(url string) []string {
 	fmt.Println(url)
 	select {
 	case tokens <- struct{}{}:
-	//case <-done:
-	//	log.Print("in crawl methoed :done ")
-	//	return nil
+	case <-done:
+		log.Print("in crawl methoed :done ")
+		return nil
 	}
 	list, err := links.Extract(url)
 	<-tokens
