76a9c4a13d199385561de81726e781b9446295d4
chiiia12 <llnsbchsep12@gmail.com>
2018-02-17 20:13:40 +0900

    ch09/memo add memo2

--- tmp/ch09/memo1.go
+++ tmp/ch09/memo1.go
@@ -1,7 +1,10 @@
 package ch09
 
+import "sync"
+
 type Memo struct {
 	f     Func
+	mu    sync.Mutex
 	cache map[string]result
 }
 type Func func(key string) (interface{}, error)
@@ -16,10 +19,12 @@ func New(f Func) *Memo {
 }
 
 func (memo *Memo) Get(key string) (interface{}, error) {
+	memo.mu.Lock()
 	res, ok := memo.cache[key]
 	if !ok {
 		res.value, res.err = memo.f(key)
 		memo.cache[key] = res
 	}
+	memo.mu.Unlock()
 	return res.value, res.err
 }
