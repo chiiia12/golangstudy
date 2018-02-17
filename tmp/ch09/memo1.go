bd7f23f3cb2846914a83cf3f5a653ce8b74d9b02
chiiia12 <llnsbchsep12@gmail.com>
2018-02-17 20:40:59 +0900

    ch09/memo fix to memo5

--- tmp/ch09/memo1.go
+++ tmp/ch09/memo1.go
@@ -1,11 +1,15 @@
 package ch09
 
-import "sync"
-
+type request struct {
+	key      string
+	response chan<- result
+}
+type entry struct {
+	res   result
+	ready chan struct{}
+}
 type Memo struct {
-	f     Func
-	mu    sync.Mutex
-	cache map[string]result
+	requests chan request
 }
 type Func func(key string) (interface{}, error)
 
@@ -15,18 +19,41 @@ type result struct {
 }
 
 func New(f Func) *Memo {
-	return &Memo{f: f, cache: make(map[string]result)}
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
 }
 
 func (memo *Memo) Get(key string) (interface{}, error) {
-	memo.mu.Lock()
-	res, ok := memo.cache[key]
-	memo.mu.Unlock()
-	if !ok {
-		res.value, res.err = memo.f(key)
-		memo.mu.Lock()
-		memo.cache[key] = res
-		memo.mu.Unlock()
-	}
+	response := make(chan result)
+	memo.requests <- request{key, response}
+	res := <-response
 	return res.value, res.err
+}
+
+func (memo *Memo) Close() {
+	close(memo.requests)
+}
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key)
+		}
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string) {
+	e.res.value, e.res.err = f(key)
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	<-e.ready
+	response <- e.res
 }
