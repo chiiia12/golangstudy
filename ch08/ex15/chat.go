0360f1a6c05391e980212fed8598cb7e91fe2335
chiiia12 <llnsbchsep12@gmail.com>
2018-02-17 16:19:06 +0900

    ch08/ex15

---
+++ ch08/ex15/chat.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"net"
+	"log"
+	"bufio"
+	"fmt"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+type client chan<- string
+
+type clientset struct {
+	c    client
+	name string
+}
+
+var (
+	entering = make(chan clientset)
+	leaving  = make(chan clientset)
+	messages = make(chan string) //クライアントから受信するすべてのメッセージ
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) //すべての接続されているクライアント
+	for {
+		select {
+		case msg := <-messages:
+			//受信するメッセージをすべてのクライアントの送信用メッセージチャネルへブロードキャストする
+			for cli := range clients {
+				select {
+				case cli <- msg:
+				default:
+					
+				}
+			}
+
+		case cli := <-entering:
+			clients[cli.c] = true
+		case cli := <-leaving:
+			delete(clients, cli.c)
+			close(cli.c)
+		}
+	}
+}
+func handleConn(conn net.Conn) {
+	fmt.Fprintln(conn, "handleConn")
+	ch := make(chan string) //送信用のクライアントメッセージ
+	ok := make(chan struct{})
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	entering <- clientset{ch, who}
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+
+	go func(conn net.Conn) {
+		for {
+			select {
+			case <-time.After(5 * time.Minute):
+				conn.Close()
+				return
+			case <-ok:
+				//なにもしない
+			}
+		}
+	}(conn)
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		ok <- struct{}{}
+		messages <- who + ": " + input.Text()
+	}
+	leaving <- clientset{ch, who}
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) //ネットワークエラーを無視
+	}
+}
---
+++ ch08/ex15/netcat3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net"
+	"log"
+	"io"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tcpConn, ok := conn.(*net.TCPConn)
+	if !ok {
+		log.Fatal("this is not tcp connection")
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{}
+	}()
+	mustCopy(conn, os.Stdin)
+	tcpConn.CloseWrite()
+	conn.Close()
+	<-done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
