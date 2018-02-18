e19ea4839f71945661ace47c64d61e434b22f997
chiiia12 <llnsbchsep12@gmail.com>
2018-02-18 13:17:26 +0900

    ch09/ex04 add output example

--- ch09/ex04/main.go
+++ ch09/ex04/main.go
@@ -33,3 +33,10 @@ func main() {
 }
 
 //観測したgoroutine数は37000くらい
+//===OUTPUT EXAMPLE===
+//goroutineCount is 491 costtime is 1.394µs
+//goroutineCount is 492 costtime is 4.13µs
+//goroutineCount is 493 costtime is 1.418µs
+//goroutineCount is 494 costtime is 7.522µs
+//goroutineCount is 495 costtime is 1.717µs
+
