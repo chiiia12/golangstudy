59321237a8ce09dafcf2077972cffe58fd10344e
chiiia12 <llnsbchsep12@gmail.com>
2018-02-18 16:50:16 +0900

    ch09/ex05 count connection

--- ch09/ex05/main.go
+++ ch09/ex05/main.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"sync"
 )
 
 func main() {
 
 	msg := make(chan struct{})
-	done := make(chan struct{})
-	done2 := make(chan struct{})
+	done := make(chan struct{}, 2)
+	end := make(chan struct{})
+
+	countConnection := 0
+	var mu sync.Mutex
+
+	go func() {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Printf("connection count is %v\n", countConnection)
+			end <- struct{}{}
+		}
+	}()
 
 	go func(done chan struct{}, msg chan struct{}) {
-		fmt.Println("msg has sended")
+		defer func() { done <- struct{}{} }()
 		msg <- struct{}{}
-		done <- struct{}{}
+		for {
+			select {
+			case <-end:
+				fmt.Println("end")
+				return
+			default:
+				<-msg
+				msg <- struct{}{}
+				mu.Lock()
+				countConnection++
+				mu.Unlock()
+			}
+		}
 	}(done, msg)
 
 	go func(done chan struct{}, msg chan struct{}) {
-		<-msg
-		fmt.Println("msg has received")
-		done2 <- struct{}{}
+		defer func() { done <- struct{}{} }()
+		for {
+			select {
+			case <-end:
+				fmt.Println("end")
+				return
+			default:
+				<-msg
+				msg <- struct{}{}
+				mu.Lock()
+				countConnection++
+				mu.Unlock()
+
+			}
+		}
 	}(done, msg)
+
 	<-done
-	<-done2
+	fmt.Println("done")
 	//ココで待ち合わせしておかないとgoroutineの処理をする前にmainスレッドが終わっちゃう。
 	// ココが終わっちゃうと他のゴルーチンが動いていても何も吐き出されない
 }
