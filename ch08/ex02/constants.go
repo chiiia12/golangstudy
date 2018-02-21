4d8629e78240722da7ff0acf1a148809fe4c4a82
chiiia12 <llnsbchsep12@gmail.com>
2018-02-21 18:22:43 +0900

    ch08/ex02 add cd command

--- ch08/ex02/constants.go
+++ ch08/ex02/constants.go
@@ -1,11 +1,18 @@
 package main
 
 const (
-	UserName     = "USER user"
-	Password     = "PASS 12345"
-	ReadyForUser = 220
-	NeedPassword = 331
-	UserLoggedIn = 230
-	NotLoggedIn  = 530
-	SyntaxError  = 501
+	UserName          = "USER user"
+	Password          = "PASS 12345"
+	ReadyForUser      = 220
+	NeedPassword      = 331
+	UserLoggedIn      = 230
+	NotLoggedIn       = 530
+	SyntaxError       = 501
+	SystemType        = 215
+	SystemStatus      = 211
+	PathNameCreated   = 257
+	EnteringPassive   = 229
+	TransferStarting  = 125
+	ClosingConnection = 226
+	CommandSucceeded  = 250
 )
--- ch08/ex02/ftp.go
+++ ch08/ex02/ftp.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"fmt"
 	"bufio"
+	"strings"
+	"path/filepath"
 )
 
 func main() {
@@ -34,6 +36,7 @@ type ConnectionManager struct {
 	out  chan string
 	ack  chan struct{}
 	done chan struct{}
+	dir  string
 }
 
 func handleConn(conn net.Conn) {
@@ -44,28 +47,51 @@ func handleConn(conn net.Conn) {
 		out:  make(chan string),
 		ack:  make(chan struct{}),
 		done: make(chan struct{}),
+		dir:  "/",
 	}
 	cm.Init()
-
 	cm.Login()
+	cm.HandleCommand()
 
-	cm.out <- "sample output"
-	log.Println("out sended")
-	<-cm.ack
-	log.Println("ack")
-	log.Println(<-cm.in)
-	log.Println("cm.in")
-	log.Println(<-cm.in)
-	log.Println("cm.in")
-
-	log.Println(<-cm.in)
-	log.Println("cm.in")
-
-	<-cm.done
-	log.Println("done")
 }
 
+func (cm *ConnectionManager) HandleCommand() {
+	for {
+		msg := <-cm.in
+		log.Println(msg)
+		command := strings.Split(msg, " ")
+		switch(command[0]) {
+		case "SYST":
+			cm.Send(SystemType, "UNIX Type: L8")
+		case "FEAT":
+			cm.Send(SystemStatus, "End FEAT.")
+		case "PWD":
+			cm.Send(PathNameCreated, "\""+cm.dir+"\" is the current directory.")
+		case "EPSV":
+			cm.Send(EnteringPassive, "Entering extended passive mode")
+			//つなぎにいく？
+			//conn, err := net.Dial("tcp", cm.conn.RemoteAddr().String()+":")
+		case "PORT":
+			log.Println("port")
+		case "LIST":
+			cm.Send(TransferStarting, "Data connection already open.Transfer starting.")
+			//files, err := ioutil.ReadDir("/")
+			//if err != nil {
+			//	log.Println(err)
+			//}
+			//for v := range files {
+			//	log.Println(v)
+			//}
+			//何かのデータを転送する
+			cm.Send(ClosingConnection, "Transfer complete")
 
+		case "CWD":
+			cm.dir = filepath.Join(cm.dir, command[1])
+			log.Println(cm.dir)
+			cm.Send(CommandSucceeded, fmt.Sprintf("%s is the current directory.", cm.dir))
+		}
+	}
+}
 func (cm *ConnectionManager) Init() {
 	go func() {
 		defer close(cm.done)
@@ -93,7 +119,7 @@ func (cm *ConnectionManager) Init() {
 func (cm *ConnectionManager) Login() {
 	cm.Send(ReadyForUser, "Service ready for new user")
 	inputUserName := <-cm.in
-	log.Println("input is ", inputUserName)
+	log.Println(inputUserName)
 	if inputUserName != UserName {
 		cm.Send(SyntaxError, "syntax error")
 	}
@@ -104,8 +130,10 @@ func (cm *ConnectionManager) Login() {
 		cm.Send(SyntaxError, "syntax error")
 	}
 	cm.Send(UserLoggedIn, "User logged in, proceed.")
+	log.Println("login succeeded")
 }
 func (cm *ConnectionManager) Send(statusCode int, msg string) {
-	fmt.Fprintf(cm.conn, "%d %s\n", statusCode, msg)
+	//fmt.Fprintf(cm.conn, "%d %s\n", statusCode, msg)
+	cm.out <- fmt.Sprintf("%d %s\n", statusCode, msg)
+	<-cm.ack
 }
-
