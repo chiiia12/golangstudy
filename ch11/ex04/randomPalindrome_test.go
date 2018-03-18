428c8233235dc52fe428606b11e4cec33d48caa8
chiiia12 <llnsbchsep12@gmail.com>
2018-03-18 18:11:01 +0900

    ch11/ex04

---
+++ ch11/ex04/randomPalindrome_test.go
@@ -0,0 +1,38 @@
+package ex03
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	switch n % 2 {
+	case 0:
+		return string(runes) + ","
+	case 1:
+		return string(runes) + " "
+	default:
+		return string(runes)
+	}
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q)=false", p)
+		}
+	}
+}
